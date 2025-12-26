3bf2841c6fdb59b8979d0e41b561ec843c644be0
emmanuel <emmalux2@gmail.com>
2025-12-26 01:02:38 +0100

    Add Terraform configuration for t2.micro EC2

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  user_data = file("../scripts/install_nginx.sh")
+
+  tags = {
+    Name = "t2micro-devops-server"
+  }
+}
